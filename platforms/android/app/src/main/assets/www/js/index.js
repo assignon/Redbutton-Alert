7a5edd854a49381db7fee7e1b861169f6a546f99
Benjamin van Rossum <benjaminvanrossum@Stages-iMac.local>
2018-03-21 17:20:39 +0100

    first commit on 21/03/18

---
+++ platforms/android/app/src/main/assets/www/js/index.js
@@ -0,0 +1,13 @@
+
+var app = {
+    // Application Constructor
+    initialize: function() {
+
+      document.addEventListener('deviceready', Animations.lauchScreen, false);
+      Animations.lauchScreen();
+
+    },
+
+};
+
+app.initialize();
---
+++ www/js/collections.js
@@ -0,0 +1,26 @@
+var Collections = {
+
+  mongogx = function()
+  {
+
+     let mongogx = new OGX.Mongogx();
+     return mongogx;
+
+     /*mongogx.createDatabase('my_project');
+   	mongogx.setDatabase('my_project');
+
+   	mongogx.createCollection('users');
+
+   	mongogx.setCollection('users');
+
+
+
+   	mongogx.insert({first_name:'Eric', age:42, sex:'male', location:{state:'ON', city:'Toronto'}, favorites:{meals:[{name:'pizza'}, {name:'pasta'}]}});
+   	mongogx.insert({first_name:'Tania', age:38, sex:'female', location:{state:'ON', city:'Toronto'}});
+   	mongogx.insert({first_name:'Julien', age:45, sex:'male', location:{state:null, city:'Stockholm'}});
+   	mongogx.insert({first_name:'George', age:55, sex:'male', location:{state:'QC', city:'Montreal'}, arts:{martial:['kickboxing', 'wresting']}});*/
+
+
+  }
+
+}
